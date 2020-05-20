8b5a466f0410158a01b40050646062b5166c5d96
szivkovic12 <stjzivkovic93@gmail.com>
2020-05-21 01:49:33 +0200

    zadnja promjena

--- src/app/employee.model.ts
+++ src/app/employee.model.ts
@@ -1,4 +1,5 @@
 export class Employee{
+    id:number;
     name:string;
     surname:string;
     oib:string;
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -9,6 +9,7 @@ import { Employee } from './employee.model';
 })
 export class EmployeeService {
   private employeeUrl = `http://localhost:8080/employee/`;
+  private employeeAddUrl = `http://localhost:8080/employee/add`;
   constructor(
     private http: HttpClient
   ) { }
@@ -20,6 +21,24 @@ export class EmployeeService {
         catchError(this.handleError<Employee[]>('getEmployee', []))
       );
   }
+
+  addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(this.employeeAddUrl, employee).pipe(
+      tap((newEmployee: Employee) => console.log(`added employee w/ ID=${newEmployee.id}`)),
+      catchError(this.handleError<Employee>('addEmployee'))
+    );
+  }
+
+  deleteEmployee(employee: Employee | number): Observable<Employee> {
+    const id = typeof employee === 'number' ? employee : employee.id;
+    const url = `${this.employeeUrl}/${id}`;
+
+    return this.http.delete<Employee>(url).pipe(
+      tap(_ => console.log(`deleted employee id=${id}`)),
+      catchError(this.handleError<Employee>('deleteEmployee'))
+    );
+  }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(operation);
